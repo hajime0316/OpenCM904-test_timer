3b3e2508dffccec7cfd8d385d0e43374f8d043d6
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-08-10 00:06:30 +0900

    Add macro for period

--- test_timer.ino
+++ test_timer.ino
@@ -1,4 +1,5 @@
-// Toggle LED on pin 13 each second
+
+#define PERIOD ((uint32)1000000)  // microsec
 
 void flash() {
   static boolean output = HIGH;
@@ -11,7 +12,7 @@ void setup() {
   pinMode(BOARD_LED_PIN, OUTPUT);
   const int channel = 1;    // channeは1を使う
   Timer1.pause();
-  Timer1.setPeriod((uint32)1000000);   // 周期1000000microsec
+  Timer1.setPeriod(PERIOD);   // 周期設定
   Timer1.setCompare(channel, Timer1.getOverflow());
   Timer1.attachInterrupt(channel, flash); // 割り込み関数設定
   Timer1.refresh();
